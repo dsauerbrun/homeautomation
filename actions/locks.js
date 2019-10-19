7722120c33266f6117f16b98b962690ddf7e646f
dan <dan@atlas-x.com>
2019-10-19 10:59:10 -0600

    add auto-lock after people unlock

--- actions/locks.js
+++ actions/locks.js
@@ -24,7 +24,12 @@ async function unlockDoors(req, res) {
   if (codeValid) {
     https.get(process.env.UNLOCK_URL, function(response) {
       if (response.statusCode == 200) {
-        res.send('Door Successfully Opened, if you still cannot enter please call Dan at ' + process.env.ERROR_PHONE);
+        res.send('Door Successfully Opened and will automatically lock in 5 minutes, if you still cannot enter please call Dan at ' + process.env.ERROR_PHONE);
+        setTimeout(function() {
+         // auto-lock the door after 5 minutes(I do this because people keep testing the code and leaving the door unlocked)
+          https.get(process.env.LOCK_URL, function() {});
+
+        }, 300000);
       } else {
         res.status(500).send('Error Opening Door, please call Dan at ' + process.env.ERROR_PHONE);
       }
