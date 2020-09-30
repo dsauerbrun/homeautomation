7095535e0cfb5983c77a8e5c473761a8079fccc0
dsauerbrun <dsauerbrun@gmail.com>
2020-09-30 08:48:52 -0600

    axios fixes

--- actions/locks.js
+++ actions/locks.js
@@ -7,8 +7,8 @@ async function lockDoors(req, res) {
 
   var codeValid = code == process.env.UNLOCK_CODE || await codeHelper.codeValid(code);
   if (codeValid) {
-    axios.post(process.env.LOCK_URL, function(response) {
-      if (response.statusCode == 200) {
+    axios.post(process.env.LOCK_URL).then(function(response) {
+      if (response.status == 200) {
         res.send('Door Successfully Locking');
       } else {
         res.status(500).send('Error Locking Door, please call Dan at ' + process.env.ERROR_PHONE);
@@ -23,17 +23,17 @@ async function unlockDoors(req, res) {
 
   var codeValid = code == process.env.UNLOCK_CODE || await codeHelper.codeValid(code);
   if (codeValid) {
-    axios.post(process.env.UNLOCK_URL, function(response) {
-      if (response.statusCode !== 200) {
+    axios.post(process.env.UNLOCK_URL).then(function(response) {
+      if (response.status !== 200) {
         res.status(500).send('Error Opening Door, please call Dan at ' + process.env.ERROR_PHONE);
       }
     });
-    axios.post(process.env.UNBOLT_URL, function(response) {
-      if (response.statusCode == 200) {
+    axios.post(process.env.UNBOLT_URL).then(function(response) {
+      if (response.status == 200) {
         res.send('Door Successfully Opened and will automatically lock in 5 minutes, if you still cannot enter please call Dan at ' + process.env.ERROR_PHONE);
         setTimeout(function() {
          // auto-lock the door after 5 minutes(I do this because people keep testing the code and leaving the door unlocked)
-          axios.post(process.env.LOCK_URL, function() {});
+          axios.post(process.env.LOCK_URL).then(function() {});
 
         }, 300000);
       } else {
